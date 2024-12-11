5cd6c25dec75d92c5dc78dfee6cbbf21eeb6a233
Tarun kale <130459422+tarunkumarkale@users.noreply.github.com>
2024-12-11 05:44:09 +0000

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Profile from "./Profile";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">hello tarun kale </h1>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profile/1">Profile 1</Link>
+          </li>
+          <li>
+            <Link to="/profile/2">Profile 2</Link>
+          </li>
+          <li>
+            <Link to="/profile/3">Profile 3</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/profile/:userId" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/Components/About.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="bg-green-500  w-[100%] h-auto ">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+      dignissimos beatae culpa blanditiis totam assumenda, earum placeat. Eius
+      vel pariatur libero sed. Iste velit officia earum perferendis voluptate
+      illum repudiandae a, atque distinctio exercitationem quisquam quo eos
+      numquam temporibus maiores nihil explicabo ipsam quae. Explicabo dolorem
+      repellendus ex nihil consectetur soluta est, delectus quidem quo velit
+      temporibus eveniet magnam exercitationem corporis, adipisci dolor, animi
+      culpa totam iure rem possimus error quasi! Temporibus repellat, eveniet
+      soluta eum dolore error obcaecati, minus expedita quisquam consectetur
+      nostrum aspernatur beatae, autem nulla aut quia? Architecto earum minima
+      accusamus at esse. Obcaecati illum possimus neque perferendis deserunt,
+      voluptates provident iusto tempora soluta veritatis ratione. Tenetur
+      suscipit rerum corrupti ipsam maiores culpa cum quaerat? Eaque dolor
+      explicabo nihil reprehenderit quae. Totam fuga ratione voluptatem nobis
+      corporis recusandae veniam cupiditate odio dignissimos beatae ullam
+      explicabo, cumque in inventore, eligendi dicta tempore accusantium,
+      voluptate illum architecto qui eius! Omnis magni ea, odio alias sed
+      voluptas vel odit dignissimos error iusto minus repellat fuga? Iste
+      similique qui vitae a distinctio, quas voluptates obcaecati minima, sint
+      officia, perspiciatis debitis repudiandae culpa libero aut deserunt
+      doloremque. Architecto inventore libero aliquid commodi minima nam unde
+      sapiente! Veritatis odit quo labore, eius modi aperiam et, ducimus quidem
+      pariatur voluptas veniam minus mollitia deserunt neque beatae aliquid
+      similique. Culpa distinctio magni cum esse sit. Esse consequuntur illo
+      assumenda. Laboriosam impedit delectus doloremque veniam eaque dicta.
+      Nihil consequatur ducimus, eum consectetur minus perspiciatis et
+      reprehenderit fugiat laborum blanditiis quasi repellat maiores optio
+      asperiores numquam veniam, totam voluptates sed neque sapiente modi ullam
+      magnam! Libero voluptatem quos consequatur, quia fugit illo quod veniam
+      tempore eum cumque, necessitatibus doloremque praesentium earum magni
+      maiores delectus exercitationem aliquid totam voluptates amet eos dolor
+      fuga nihil molestias. Molestiae ut nobis aperiam laboriosam repellendus a
+      quam doloremque corporis, rerum nisi modi error impedit voluptatem
+      excepturi blanditiis rem at asperiores dolorum reprehenderit fuga,
+      similique hic. Dolor placeat earum, dicta voluptates eum molestiae
+      corrupti ratione quod atque excepturi odit inventore voluptatem similique
+      ea harum sunt necessitatibus? Autem ex ducimus incidunt maxime dolorum,
+      eveniet ea. Ipsam illum voluptatem nulla molestias nemo ut architecto
+      accusantium, nesciunt similique accusamus quam repudiandae iure dolores
+      tempora odio minus ex consequuntur eos beatae vero perspiciatis dolor eum
+      molestiae. Laudantium ducimus dolor ullam nihil dolores labore veritatis
+      natus beatae suscipit eligendi ad, repudiandae possimus, quam quaerat
+      nulla quis alias ut unde porro impedit, libero itaque. Blanditiis eligendi
+      nemo repellendus incidunt non, magni accusamus ratione. Dolorum ducimus,
+      magni provident dolores esse dicta quas sequi nihil iste architecto minima
+      ad facere libero voluptate accusamus ut saepe molestias est tempora animi
+      repellat. Rerum ratione eligendi itaque? Similique quae voluptate,
+      molestiae numquam, amet expedita recusandae veniam tempore nesciunt
+      corrupti incidunt, iure nihil provident. Ullam vitae pariatur
+      consequuntur, repellat aliquid minima quidem illo, fugiat, laudantium
+      obcaecati quis similique. Quos, fugiat distinctio ab cupiditate tenetur
+      ratione in sequi inventore, tempora optio placeat, explicabo repudiandae
+      deleniti earum. Voluptatem itaque voluptas alias optio magnam, ex
+      voluptates quia corporis harum! Molestias sequi totam rerum repellat?
+      Voluptas facere doloribus aliquid! Nam, nihil nulla cum est architecto
+      inventore praesentium. Saepe ab eligendi aperiam debitis neque repellendus
+      corrupti delectus reprehenderit nihil ipsa obcaecati sed dolores quos
+      nesciunt, quis facilis maxime. Aut doloremque libero ipsa dolore
+      accusantium neque, porro ducimus tenetur cupiditate consectetur esse
+      cumque consequuntur? Saepe accusamus, nobis aspernatur omnis praesentium
+      debitis. Reiciendis illum illo sapiente adipisci exercitationem provident
+      sint quos ratione tenetur consequuntur perferendis reprehenderit quam cum
+      minima veritatis eligendi, maxime, officia laudantium aliquam ipsum, iste
+      odit esse. Quaerat consequuntur quo nostrum quae minima fugit sed, ratione
+      fuga commodi dolore cupiditate magni inventore beatae corrupti, obcaecati
+      sequi iste aperiam? Eligendi, amet?
+    </div>
+  );
+};
+
+export default About;
